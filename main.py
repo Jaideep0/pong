3ae91fe79fd17bce3e3e0929ad1e46fde94cff89
Jaideep Singh <89267664+Jaideep0@users.noreply.github.com>
2024-03-23 00:33:47 +0530

    How to Detect when the Ball goes Out of Bounds

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from paddle import Paddle
 from ball import Ball
 import time
 
-
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
-
 
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
@@ -18,11 +16,29 @@ ball = Ball()
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1  )
+    time.sleep(0.1)
     screen.update()
     ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # Ball needs to bounce
+        ball.y_bounce()
+    # elif ball.xcor() > 380 or ball.xcor() < -380:
+    #     ball.x_bounce()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.x_bounce()
+
+    if ball.xcor() > 390:
+        ball.reset_position()
+
+    if ball.xcor() < -390:
+        ball.reset_position()
 
 screen.exitonclick()
